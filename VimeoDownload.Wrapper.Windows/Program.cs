1cc12582ff0ce4ed2559320a704a5b3e8c777f2c
djjsdtc <junjie.ding@ivybridge.cn>
2019-09-20 16:49:02 +0800

    Small fix.

--- VimeoDownload.Wrapper.Windows/Program.cs
+++ VimeoDownload.Wrapper.Windows/Program.cs
@@ -1,12 +1,9 @@
 ﻿namespace VimeoDownload.Wrapper.Windows
 {
-    using System;
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(string.Join(" ", args));
             ProgramEntrance.Main(args);
         }
     }
